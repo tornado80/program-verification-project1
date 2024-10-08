96b9168a91b8e80a294b356480bb515491c3f395
Federico Marra <fecrom@gmail.com>
2024-10-08 21:28:10 +0200

    Implement match & nondet

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use ivl::{IVLCmd, IVLCmdKind, WeakestPrecondition};
 use slang::ast::{Cmd, CmdKind, Expr, ExprKind, Ident, Name, Op, Type};
 use slang_ui::prelude::*;
 use std::cell::Cell;
+use slang_ui::prelude::slang::ast::{Cases, PrefixOp};
 
 thread_local!(static HAVOC_COUNTER: Cell<i32> = Cell::new(0));
 
@@ -77,24 +78,46 @@ fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
         CmdKind::VarDefinition { name, ty:(_, typ) , expr:None  } => Ok(havoc_to_ivl(name, typ)),
         CmdKind::VarDefinition { name, ty:_, expr:Some(value)  } => Ok(IVLCmd::assign(name, value)),
         CmdKind::Assignment { name, expr } => Ok(IVLCmd::assign(name, expr)),
+        CmdKind::Match {body} => match_to_ivl(body),
         _ => todo!("{:#?}", cmd.kind),
     }
 }
 
-fn havoc_to_ivl(name: &Name, typ: &Type) -> IVLCmd{
+fn match_to_ivl(body: &Cases) -> Result<IVLCmd> {
+    let first_case = body.cases[0].clone();
+    let cmd_b: IVLCmd = IVLCmd::assume(&first_case.condition).seq(&cmd_to_ivlcmd(&first_case.cmd)?);
+    if body.cases.len() == 1 {
+        return Ok(cmd_b);
+    }
+    let new_body = Cases{ span: body.span, cases: body.cases[1 .. body.cases.len()].to_vec() };
+    let cmd_not_b: IVLCmd = IVLCmd::assume(&Expr::new_typed(ExprKind::Prefix(PrefixOp::Not,Box::new(first_case.condition.clone())), Type::Bool)).seq(&match_to_ivl(&new_body)?);
+    return Ok(cmd_b.nondet(&cmd_not_b));
+}
+
+fn havoc_to_ivl(name: &Name, typ: &Type) -> IVLCmd {
     HAVOC_COUNTER.set(HAVOC_COUNTER.get() + 1);
     return IVLCmd::assign(name, &Expr::new_typed(ExprKind::Ident(Ident(format!("havoc{0}", HAVOC_COUNTER.get()).to_string())), typ.clone()))
 }
 
 fn wp_set(ivl: &IVLCmd, post_conditions: Vec<WeakestPrecondition>) -> Result<Vec<WeakestPrecondition>> {
     match &ivl.kind {
-        IVLCmdKind::Assert { condition, message } => Ok([post_conditions, vec![WeakestPrecondition { expr: condition.clone(), span: condition.span, msg: message.clone() }]].concat()),
+        IVLCmdKind::Assert { condition, message } => wp_set_assert(condition, message, post_conditions),
         IVLCmdKind::Assume { condition } => wp_set_assume(condition, post_conditions),
         IVLCmdKind::Seq(cmd1, cmd2) => wp_set_seq(cmd1, cmd2, post_conditions),
-        IVLCmdKind::Assignment { name, expr } => Ok(wp_set_assignment(name, expr, post_conditions)),
-
+        IVLCmdKind::Assignment { name, expr } => wp_set_assignment(name, expr, post_conditions),
+        IVLCmdKind::NonDet(cmd1, cmd2) => wp_set_nondet(cmd1, cmd2, post_conditions),
         _ => todo!("Not supported in wp (yet)."),
     }
+}
+
+fn wp_set_nondet(cmd1: &Box<IVLCmd>, cmd2: &Box<IVLCmd>, post_condition: Vec<WeakestPrecondition>) -> Result<Vec<WeakestPrecondition>> {
+    let wp_set1 = wp_set(cmd1, post_condition.clone())?;
+    let wp_set2 = wp_set(cmd2, post_condition.clone())?;
+    return Ok([wp_set1, wp_set2].concat());
+}
+
+fn wp_set_assert(condition: &Expr, message: &String, post_conditions: Vec<WeakestPrecondition>) -> Result<Vec<WeakestPrecondition>> {
+    return Ok([post_conditions, vec![WeakestPrecondition { expr: condition.clone(), span: condition.span, msg: message.clone() }]].concat());
 }
 
 fn wp_set_assume(condition: &Expr, post_conditions: Vec<WeakestPrecondition>) -> Result<Vec<WeakestPrecondition>> {
@@ -112,14 +135,13 @@ fn wp_set_seq(cmd1: &Box<IVLCmd>, cmd2: &Box<IVLCmd>, post_condition: Vec<Weakes
     return Ok(wp_set1);
 }
 
-fn wp_set_assignment(identifier: &Name, indentifier_value: &Expr, conditions: Vec<WeakestPrecondition>) -> Vec<WeakestPrecondition> {
+fn wp_set_assignment(identifier: &Name, indentifier_value: &Expr, conditions: Vec<WeakestPrecondition>) -> Result<Vec<WeakestPrecondition>> {
     let mut updated_conditions: Vec<WeakestPrecondition> = vec![];
     for WeakestPrecondition { expr, span, msg } in conditions {
         let new_condition = replace_in_expression(&expr, identifier, indentifier_value);
         updated_conditions.push(WeakestPrecondition { expr: new_condition, span: span, msg: msg });
     }
-
-    return updated_conditions;
+    return Ok(updated_conditions);
 }
 // f (e, i, v) -> e[i <- v]
 fn replace_in_expression(origin_expression: &Expr, identifier: &Name, identifier_value: &Expr) -> Expr {
